4acdb024f7f3dc87b8868dab09dba82ac9f2595a
prakashjena938 <144409335+prakashjena938@users.noreply.github.com>
2026-01-28 11:14:39 +0530

    here is the proof of Heterogenious Object are allowed &&  null insertion is possible && has the ReSizAble Array || GrowAble Array && allows the duplicate insertion && insertion order preserved

--- List/ARRAYLIST/ARRAYLIST1.java
+++ List/ARRAYLIST/ARRAYLIST1.java
@@ -1,6 +1,28 @@
 package List.ARRAYLIST;
 
+import java.util.ArrayList;
+
 public class ARRAYLIST1
  {
-    
+    /*
+    1.It has the ReSizAble Array || GrowAble Array.
+    2.It allows the duplicate insertion.
+    3.Here the insertion order preserved. (in which order you inserts in that order you can delete).
+    4.Here the Heterogenious Object are allowed.(acept)
+    5.Here the null insertion is possible.
+    6.Incuding ArrayList every Collection are Serialisable && Cloneable Interface.
+    */
+
+// @SuppressWarnings("rawtypes")
+public static void main(String[] args) {
+    ArrayList<Object> a = new ArrayList<>();
+    a.add("prakash");
+    a.add(9);
+    a.add(true);
+    a.add('k');
+    a.add(9.785);
+    System.out.println(a);
+
+}
+
 }
